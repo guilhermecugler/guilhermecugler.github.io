8db93621166d635d0f8bd15f6dd2a8c866be0593
Guilhermme Cugler <guilhermecugler@gmail.com>
2024-08-19 17:15:38 -0300

    add new app

--- tv/script.js
+++ tv/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apks = [
+        "Aplicativo NOVO Veloznet XC.apk",
         "P2CINE Principal.apk",
         "TurboBlack.apk",
         "Evolution.apk",
